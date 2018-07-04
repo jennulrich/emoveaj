ec07e3693bad18cc16292796b5b5c09c8fff9228
Jennifer Calipel <jennifer.calipel@gmail.com>
2018-07-04 16:56:21 +0200

    ajout de l'entité cart

---
+++ src/AppBundle/Entity/Cart.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cart
+ *
+ * @ORM\Table(name="cart")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CartRepository")
+ */
+class Cart
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="ordered_at", type="datetime")
+     */
+    private $orderedAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="token", type="string", length=60)
+     */
+    private $token;
+
+
+    /**
+     * Cart constructor.
+     */
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->cartProducts = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Cart
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set orderedAt
+     *
+     * @param \DateTime $orderedAt
+     *
+     * @return Cart
+     */
+    public function setOrderedAt($orderedAt)
+    {
+        $this->orderedAt = $orderedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get orderedAt
+     *
+     * @return \DateTime
+     */
+    public function getOrderedAt()
+    {
+        return $this->orderedAt;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     *
+     * @return Cart
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+}
+

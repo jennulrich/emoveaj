15d6a087a88374f85e18d9e229b517d09fd3cf65
jennulrich <31631277+jennulrich@users.noreply.github.com>
2018-07-04 17:19:31 +0200

    ajout de l'entité cart product (#7)

---
+++ src/AppBundle/Entity/CartProduct.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CartProduct
+ *
+ * @ORM\Table(name="cart_product")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CartProductRepository")
+ */
+class CartProduct
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer")
+     */
+    private $quantity;
+    
+    /**
+     * CartProduct constructor.
+     */
+    public function __construct()
+    {
+        $this->quantity = 1;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     *
+     * @return CartProduct
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+}
+

ccdf0746a8b49b9225b0c60aca06d781cfbde16d
Amandine <amandine.lesaint@yahoo.com>
2018-08-02 15:36:37 +0200

    envoi branch

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amandine
+ * Date: 06/07/2018
+ * Time: 12:09
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Home controller.
+ *
+ * @Route("home")
+ */
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function HomeAction()
+    {
+        return $this->render('front/home.html.twig');
+    }
+}

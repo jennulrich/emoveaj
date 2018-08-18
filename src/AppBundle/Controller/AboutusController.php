bfee59ac358bbfdb1ff76344c3aeb3762a703c18
AmandineL <amandine.lesaint@yahoo.com>
2018-08-18 19:08:53 +0200

    page about us + lien avec la home

---
+++ src/AppBundle/Controller/AboutusController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amandine
+ * Date: 18/08/2018
+ * Time: 17:55
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * About Us controller.
+ *
+ * @Route("front/aboutus")
+ */
+class AboutusController extends Controller
+{
+    /**
+     * @Route("/", name="aboutus")
+     */
+    public function AboutusAction()
+    {
+        return $this->render('front/aboutus.html.twig');
+    }
+}

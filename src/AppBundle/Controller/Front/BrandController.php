ec9ed4c0dca8296b3783796c0a7aa1aee3f552bc
Jennifer Calipel <jennifer.calipel@gmail.com>
2018-08-19 16:23:00 +0200

    car list ameliorations

--- src/AppBundle/Controller/Front/BrandController.php
+++ src/AppBundle/Controller/Front/BrandController.php
@@ -2,58 +2,52 @@
 
 namespace AppBundle\Controller\Front;
 
+use AppBundle\Entity\Brand;
+use AppBundle\Form\BrandType;
 use AppBundle\Manager\BrandManager;
-use AppBundle\Manager\CarManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Car controller.
+ * Brand controller.
  *
- * @Route("front/car")
+ * @Route("front/brand")
  */
-class CarController extends Controller
+class BrandController extends Controller
 {
-    /** @var CarManager */
-    private $carManager;
-
     /** @var BrandManager */
     private $brandManager;
 
-    public function __construct(CarManager $carManager, BrandManager $brandManager)
+    public function __construct(BrandManager $brandManager)
     {
-        $this->carManager = $carManager;
         $this->brandManager = $brandManager;
     }
 
     /**
-     * @Route("/{id}", name="front_view_car", requirements={"id"="\d+"})
+     * @Route("/{id}", name="front_view_brand", requirements={"id"="\d+"})
      * @param $id int
      * @return Response
      */
     public function viewAction(int $id): Response
     {
-        $car = $this->carManager->get($id);
         $brand = $this->brandManager->get($id);
 
-        return $this->render('front/car/detail.html.twig', [
-            "car" => $car,
+        return $this->render('front/brand/detail.html.twig', [
             "brand" => $brand
         ]);
     }
 
     /**
-     * @Route("/", name="front_cars")
+     * @Route("/", name="front_brands")
      * @return Response
      */
     public function listAction(): Response
     {
-        $cars = $this->carManager->getList();
         $brands = $this->brandManager->getList();
 
-        return $this->render('front/car/list.html.twig', [
-            "cars" => $cars,
+        return $this->render('front/brand/list.html.twig', [
             "brands" => $brands
         ]);
     }

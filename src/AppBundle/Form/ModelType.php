d24b05b00f181f47f58d2178d7f507f4d80290ae
jennulrich <31631277+jennulrich@users.noreply.github.com>
2018-08-19 17:04:13 +0200

    Car list ameliorations (#35)
    * car list ameliorations
    
    * probleme formtype

--- src/AppBundle/Form/ModelType.php
+++ src/AppBundle/Form/ModelType.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Brand;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -15,7 +17,7 @@ class ModelType extends AbstractType
     {
         $builder
             ->add('name_model',TextType::class, array('label' => 'Modèle'))
-            ->add('brand', TextType::class, array('label' => 'Marque'))
+            //->add('brand', TextType::class, array('label' => 'Marque'))
             ->add('gamme', TextType::class, array('label' => 'Gamme'))
             ->add('autonomie', TextType::class, array('label' => 'Autonomie'));
         //->add('image', FileType::class, array(
